aa5ae7d5c63a25c270d0c1f805b8aaca2cf64f1c
laurenstrapagiel <lauren.strapagiel@gmail.com>
2024-03-13 16:02:37 -0600

    fixes route path

--- routes/warehouses-routers.js
+++ routes/warehouses-routers.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const warehousesController = require("../controllers/warehouses-controller");
 const inventoriesController = require("../controllers/inventories-controller");
 
-router.route("/inventories").get(inventoriesController.getInventories);
+router.route("/inventory").get(inventoriesController.getInventories);
 
 router.route("/warehouses/:id").get(warehousesController.findOneWarehouse);
 
