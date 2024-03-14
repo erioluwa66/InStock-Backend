670dea84485e2d0605b0b8e556820a326fd463f0
LiZhang-Canada <zhl924505@gmail.com>
2024-03-14 09:51:39 -0600

    merge conflict solve

--- routes/warehouses-routers.js
+++ routes/warehouses-routers.js
@@ -2,15 +2,9 @@ const router = require("express").Router();
 const warehousesController = require("../controllers/warehouses-controller");
 const inventoriesController = require("../controllers/inventories-controller");
 
-<<<<<<< HEAD
-router
-    .route("/inventories")
-    .get(inventoriesController.getInventories);
-=======
 router.route("/inventory").get(inventoriesController.getInventories);
 
 router.route("/warehouses").get(warehousesController.getWarehouses);
->>>>>>> develop
 
 router
     .route("/warehouses/:id")
