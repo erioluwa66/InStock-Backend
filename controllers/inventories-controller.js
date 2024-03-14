b8d54f551b5457b4919bc6822aa79a01b7e55b87
LiZhang-Canada <zhl924505@gmail.com>
2024-03-13 21:29:53 -0600

    finish get single inventory API

--- controllers/inventories-controller.js
+++ controllers/inventories-controller.js
@@ -20,6 +20,35 @@ const getInventories = async (req, res) => {
   }
 };
 
+const getOneInventory = async (req, res) => {
+  try {
+    const data = await knex
+      .select(
+        "inventories.id",
+        "warehouses.warehouse_name",
+        "inventories.item_name",
+        "inventories.description",
+        "inventories.category",
+        "inventories.status",
+        "inventories.quantity"
+      )
+      .from("inventories")
+      .join("warehouses", "inventories.warehouse_id", "warehouses.id")
+      .where ({ "inventories.id": req.params.id });
+      
+      if (data.length === 0) {
+        return res.status(404).json({
+          message: `Inventory with ID ${req.params.id} not found` 
+        });
+      }
+
+    res.status(200).json(data[0]);
+  } catch (err) {
+    res.status(500).send(`Unable to fetch inventory data with ID ${req.params.id}`);
+  }
+};
+
 module.exports = {
   getInventories,
+  getOneInventory,
 };
--- routes/warehouses-routers.js
+++ routes/warehouses-routers.js
@@ -2,8 +2,16 @@ const router = require("express").Router();
 const warehousesController = require("../controllers/warehouses-controller");
 const inventoriesController = require("../controllers/inventories-controller");
 
-router.route("/inventories").get(inventoriesController.getInventories);
+router
+    .route("/inventories")
+    .get(inventoriesController.getInventories);
 
-router.route("/warehouses/:id").get(warehousesController.findOneWarehouse);
+router
+    .route("/warehouses/:id")
+    .get(warehousesController.findOneWarehouse);
+
+router
+    .route("/inventories/:id")
+    .get(inventoriesController.getOneInventory);
 
 module.exports = router;
