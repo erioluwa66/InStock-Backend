14a8a60ff5f1bcb84efb31ee72bc6b46222ea40e
laurenstrapagiel <lauren.strapagiel@gmail.com>
2024-03-16 11:36:58 -0600

    hot fix

--- controllers/inventories-controller.js
+++ controllers/inventories-controller.js
@@ -130,7 +130,7 @@ const editInventory = async (req, res) => {
     const updatedInventory = await knex("inventories")
     .select(
       "inventories.id",
-      "inventories.warehouse_",
+      "inventories.warehouse_name",
       "inventories.item_name",
       "inventories.description",
       "inventories.category",
